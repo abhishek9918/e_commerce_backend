f3f031b7bada4f0d3c1702c6a097924e770dceaf
abhishek9918 <ojha.abhishek312@gmail.com>
2024-12-26 17:53:29 +0530

    update atal and changesdfdsfsfsdffs

--- db/config.js
+++ db/config.js
@@ -1,17 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const mongoURI =
-//   "mongodb+srv://ojhaabhishek312:dhMhLWQNzRPJtVVz@cluster0.yw6i4.mongodb.net/e-commerce?retryWrites=true&w=majority";
-
-// mongoose
-//   .connect(mongoURI)
-//   .then(() => {
-//     console.log("Successfully connected to MongoDB Atlas");
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to MongoDB Atlas:", err);
-//   });
-
 const mongoose = require("mongoose");
 
 const mongoURI =
@@ -19,9 +5,7 @@ const mongoURI =
 
 mongoose
   .connect(mongoURI, {
-    useNewUrlParser: true, // Optional, but recommended for MongoDB connection parsing
-    useUnifiedTopology: true, // Helps with the new MongoDB driver's behavior
-    serverSelectionTimeoutMS: 30000, // Increase the timeout to 30 seconds
+    serverSelectionTimeoutMS: 30000, // Keep the extended timeout
   })
   .then(() => {
     console.log("Successfully connected to MongoDB Atlas");
