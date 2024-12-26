2f7bb243fc18fd483e29cd2298b2ed4535f3f834
abhishek9918 <ojha.abhishek312@gmail.com>
2024-12-26 19:32:55 +0530

    update ip

--- db/config.js
+++ db/config.js
@@ -1,22 +1,3 @@
-// const mongoose = require("mongoose");
-
-// // MongoDB Atlas connection string
-// const mongoURI =
-//   "mongodb+srv://ojhaabhishek312:dhMhLWQNzRPJtVVz@cluster0.yw6i4.mongodb.net/e-commerce?retryWrites=true&w=majority";
-
-// // Connect to MongoDB Atlas
-// mongoose
-//   .connect(mongoURI, {
-//     serverSelectionTimeoutMS: 50000, // Extended timeout for server selection
-//     family: 4, // Forces IPv4 usage
-//   })
-//   .then(() => {
-//     console.log("Successfully connected to MongoDB Atlas");
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to MongoDB Atlas:", err.message);
-//   });
-
 const mongoose = require("mongoose");
 
 const mongoURI =
@@ -24,7 +5,7 @@ const mongoURI =
 
 mongoose
   .connect(mongoURI, {
-    serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds instead of default 30s
+    serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
   })
   .then(() => {
     console.log("Successfully connected to MongoDB Atlas");
