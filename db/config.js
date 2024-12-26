c448aa71e776a268842bbac0bb1cf65420c483d8
abhishek9918 <ojha.abhishek312@gmail.com>
2024-12-26 17:31:25 +0530

    update atal

--- db/config.js
+++ db/config.js
@@ -1,3 +1,17 @@
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/e-commerce");
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/e-commerce");
-// users;
+
+// Replace the local connection string with the MongoDB Atlas connection string
+const mongoURI =
+  "mongodb+srv://ojhaabhishek312:dhMhLWQNzRPJtVVz@cluster0.yw6i4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
